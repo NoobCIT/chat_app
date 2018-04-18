952100038572786dc85f1ff3ac786194a5cddbcc
Kevin <nguyenk215@yahoo.com>
2018-04-18 15:35:33 -0700

    added log out and profile for home page

--- controllers/StaticPagesController.js
+++ controllers/StaticPagesController.js
@@ -3,7 +3,7 @@ var staticPagesController = {};
 
 // Home Page
 staticPagesController.home = function(req, res) {
-  res.render("../views/StaticPages/home");
+  res.render("../views/StaticPages/home", {session: req.session.userId});
 };
 
 module.exports = staticPagesController;
